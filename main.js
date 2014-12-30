fc1d51296029efee05bb987f015086aef1a81ee6
Eaug9th <Eaug9th@gmail.com>
2014-12-29 22:54:30 -0500

    lesson 3-3

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-$(document).ready(function(){
+//$(document).ready(function(){
 var f="fizz!", b="BUZZ!", fb="FizzBuzz!";
-for (var i=1; i<101; i++){
-  if(i%(3*5)==0){
-    $('<div></div>').appendTo("#fb").html('<p>' + fb + '</p>');
-    //console.log(fb);
-  }else if(i%3==0){
-    $('<div></div>').appendTo("#fb").html('<p>' + f + '</p>');
-    //console.log(f);
-  }else if (i%5==0){
-    $('<div></div>').appendTo("#fb").html('<p>' + b + '</p>');
-    //console.log(b);
-  }else {
-    //console.log(i);
-   $('<div></div>').appendTo("#fb").html('<p>' + i + '</p>');
+
+  function theFunction(){
+    var z=document.getElementById("maxNum").value;
+    var x =3; var y = 5;
+
+    z=Math.round(z);
+
+
+    for (var i=1; i<z+1; i++){
+      
+      if(i%(x*y)==0){
+        $('<div></div>').appendTo("#fb").html('<p>' + fb + '</p>');
+    
+      }else if(i%y==0){
+        $('<div></div>').appendTo("#fb").html('<p>' + f + '</p>');
+    
+      }else if (i%x==0){
+        $('<div></div>').appendTo("#fb").html('<p>' + b + '</p>');
+    
+      }else {
+    
+        $('<div></div>').appendTo("#fb").html('<p>' + i + '</p>');
+      }
+    }
   }
-}
-});
